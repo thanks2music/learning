ad147a114cdecca16b14c7457968786df63c0efc
Yoshiyuki Ito <thanks2music@gmail.com>
2024-08-09 13:01:38 +0900

    Add headline component on pages router ver

---
+++ react/with-nextjs/pages_router_ver/learn-react-with-nextjs-pages-router-ver/components/Headline.js
@@ -0,0 +1,19 @@
+// React.Fragmentは、子要素をグループ化するためのコンポーネント
+// 1. import React from "react";
+// 2. React.Fragmentを使う
+// 3. or 省略形は<>を使う
+
+export function Headline(props) {
+  return (
+    <div>
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">pages/{props.page}.js</code>
+        </p>
+      </div>
+
+      <h1 className="text-4xl font-bold text-slate-8000">{props.title}</h1>
+    </div>
+  );
+}
--- react/with-nextjs/pages_router_ver/learn-react-with-nextjs-pages-router-ver/pages/index.js
+++ react/with-nextjs/pages_router_ver/learn-react-with-nextjs-pages-router-ver/pages/index.js
@@ -1,7 +1,8 @@
-import Image from "next/image";
+// import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Footer } from "@/components/Footer";
 import { Links } from "@/components/Links";
+import { Headline } from "@/components/Headline";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,14 +11,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-      </div>
-
-      <h1 className="text-4xl font-bold text-slate-8000">Home</h1>
+      <Headline title="Home" page="index"/>
 
       <Links />
       <Footer />
