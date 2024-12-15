49b4670e1244cb6e3ebc9d0185c0519e75edbaa5
Yoshiyuki Ito <thanks2music@gmail.com>
2024-12-15 16:11:33 +0900

    Add linter in turbo

--- monorepo/turbo/apps/web/.eslintrc.js
+++ monorepo/turbo/apps/web/.eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/next.js"],
+  extends: ["@repo/eslint-config/next.js", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
