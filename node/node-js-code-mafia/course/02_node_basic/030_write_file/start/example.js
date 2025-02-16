6985dcc8f9c8854583e5465c8b6342e1279a114a
Yoshiyuki Ito <thanks2music@gmail.com>
2025-02-16 12:55:42 +0900

    Node.jsでのファイル書き込みについて

--- node/node-js-code-mafia/course/02_node_basic/030_write_file/start/example.js
+++ node/node-js-code-mafia/course/02_node_basic/030_write_file/start/example.js
@@ -1,1 +1,14 @@
-console.log('hello, node.js');
+// ファイルへの書き込み
+// __dirname, __filename
+const fs = require('fs');
+const path = require('path');
+
+const distPath = path.resolve(__dirname, '../dist/test3.txt');
+const distPath2 = path.resolve(__dirname, '..', 'dist', 'test4.txt');
+const distPath3 = path.join(__dirname, '../dist/test3.txt');
+console.log(distPath3);
+
+// 相対パスは、node.jsを実行するディレクトリからの相対パス
+// fs.writeFileSync(distPath, 'Hello, node.js');
+// fs.writeFileSync(distPath2, 'Hello, node.js');
+// console.log('hello, node.js');
