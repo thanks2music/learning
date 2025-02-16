2740d9c9fdc42610a150a5c19704446fa963237f
Yoshiyuki Ito <thanks2music@gmail.com>
2025-02-16 12:56:09 +0900

    Common.jsの場合のモジュール読み込みについて

--- node/node-js-code-mafia/course/02_node_basic/040_module_system/start/example.js
+++ node/node-js-code-mafia/course/02_node_basic/040_module_system/start/example.js
@@ -1,5 +1,10 @@
-function plus(a, b) {
-  return a + b;
-}
-const result = plus(1, 2);
+// Common.jsでモジュールを取得するには「.require()」を使う
+// ファイルをインポートする際のパスは、ファイルからの相対パスとなる他、拡張子を省略することができる
+const calc = require('./calc');
+const { plus, minus } = require('./calc');
+const result = calc.plus(1, 2);
+const result2 = plus(6, 4);
+const result3 = minus(10, 5);
 console.log(result);
+console.log(result2);
+console.log(result3);
