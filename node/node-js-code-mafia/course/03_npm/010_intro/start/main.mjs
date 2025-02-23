6579500a7631337fb2a0da336391029bee26ac07
Yoshiyuki Ito <thanks2music@gmail.com>
2025-02-23 16:08:46 +0900

    Lodashと、Live Serverを使って、ES Moduleからパッケージの読み込みを復習した所まで

--- node/node-js-code-mafia/course/03_npm/010_intro/start/main.mjs
+++ node/node-js-code-mafia/course/03_npm/010_intro/start/main.mjs
@@ -1,2 +1,8 @@
-const original = { prop: { nested: "value" } };
+import cloneDeep from 'lodash/cloneDeep.js';
+const original = { prop: { nested: 'value' } };
 // オブジェクトの複製
+const cloned = cloneDeep(original);
+
+cloned.prop.nested = 'another new value';
+
+console.log(original, cloned);
--- node/node-js-code-mafia/course/03_npm/050_npm-link/lib-prj/main.js
+++ node/node-js-code-mafia/course/03_npm/050_npm-link/lib-prj/main.js
@@ -1,3 +1,3 @@
 export default function hello() {
-    return 'hell';
+  return 'hello!';
 }
