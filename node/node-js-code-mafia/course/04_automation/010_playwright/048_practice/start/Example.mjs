5312edb48c63fa30d9fcdf082b9815161519a86f
Yoshiyuki Ito <thanks2music@gmail.com>
2025-02-23 17:14:16 +0900

    練習問題の答えと、Playwrightの基本的な使い方まで

--- node/node-js-code-mafia/course/04_automation/010_playwright/048_practice/start/Example.mjs
+++ node/node-js-code-mafia/course/04_automation/010_playwright/048_practice/start/Example.mjs
@@ -10,11 +10,29 @@ import { chromium } from '@playwright/test';
   await page.goto('http://localhost:3000');
 
   const inputLocator = page.locator('.text-center.mt-4 label input');
-  await inputLocator.type('佐藤');
+  await inputLocator.type('藤');
 
-  const satoListLocator = page.locator('.cards.list-group-item >> nth=-1');
-  const satoCount = await satoListLocator.count();
-  const lastSatoName = await satoListLocator.innerText();
-  console.log(lastSatoName);
-  await browser.close();
+  const pagerLocator = page.locator('.page-link.page-number');
+
+  if ((await pagerLocator.count()) > 1) {
+    const lastPagerLocator = pagerLocator.locator('nth=-1');
+    await lastPagerLocator.click();
+  }
+
+  // Teacher's Answer Version
+  const lastCardLocator = page.locator('.cards.list-group-item >> nth=-1');
+  // @NOTE: 「innerText()と、textContent()の違いについて」
+  // DOMのAPIで、「textContent()」と、「innerText()」プロパティが存在する。
+  // 「textContent」は、要素の中の文字列を取得するもの
+  // 「innerText」の要素の中で、表示されているものだけを取得するもの
+  const lastCardName = await lastCardLocator.textContent();
+  console.log(lastCardName);
+
+  // My Answer Version
+  // const satoListLocator = page.locator('.cards.list-group-item >> nth=-1');
+  // const satoCount = await satoListLocator.count();
+  // 「innerText」では、display: noneや、visibility: hiddenの要素は取得出来ない
+  // const lastSatoName = await satoListLocator.innerText();
+  // console.log(lastSatoName);
+  // await browser.close();
 })();
