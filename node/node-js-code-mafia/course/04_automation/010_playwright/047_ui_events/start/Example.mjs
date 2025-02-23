c1210c10645d120c5258c6925223f8d513e59c7a
Yoshiyuki Ito <thanks2music@gmail.com>
2025-02-23 17:00:16 +0900

    Lacatorで、type/click/countなどのメソッドを使用した操作方法について

--- node/node-js-code-mafia/course/04_automation/010_playwright/047_ui_events/start/Example.mjs
+++ node/node-js-code-mafia/course/04_automation/010_playwright/047_ui_events/start/Example.mjs
@@ -1,4 +1,4 @@
-import { chromium } from "@playwright/test";
+import { chromium } from '@playwright/test';
 
 // @see セレクターのチェーンの利用方法(>>)
 // https://playwright.dev/docs/selectors#chaining-selectors
@@ -6,14 +6,24 @@ import { chromium } from "@playwright/test";
 (async () => {
   const browser = await chromium.launch({ headless: false, slowMo: 500 });
   const page = await browser.newPage();
-  await page.goto("http://localhost:3000");
+  await page.goto('http://localhost:3000');
 
+  // waitForTimeoutは、デバッグ時などに使える待機させるメソッド
+  await page.waitForTimeout(2000);
   // CSS セレクターで要素を取得
-  const pageTitleLocator = await page.locator(".cards.list-group-item");
-  const parentLocator = await pageTitleLocator.locator('..')
-  const pageTitle = await parentLocator.innerHTML();
-  console.log(pageTitle);
+  // @NOTE: Locatorは、DOMとは異なり、要素を取得するためのオブジェクト
+  const inputLocator = page.locator('.text-center.mt-4 > label > input');
+  // typeメソッドで、inputを入力出来る
+  await inputLocator.type('美');
 
-  await browser.close();
+  const pager3Locator = page.locator('.page-link.page-number >> nth=-1');
+  await pager3Locator.click();
 
+  debugger;
+
+  const cardLocator = page.locator('.cards.list-group-item');
+  const cardCount = await cardLocator.count();
+  console.log(cardCount);
+
+  // await browser.close();
 })();
