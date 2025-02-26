f0e256e0c3e4205f6dfcd489ad69bea69a4c1f5a
Yoshiyuki Ito <thanks2music@gmail.com>
2025-02-26 10:34:40 +0900

    57の練習問題を解いたところまで

--- node/node-js-code-mafia/course/04_automation/010_playwright/050_write_file/start/Example.mjs
+++ node/node-js-code-mafia/course/04_automation/010_playwright/050_write_file/start/Example.mjs
@@ -12,14 +12,28 @@ import { Parser } from 'json2csv';
 
   const fetchedCards = [];
   for (let i = 0; i < cardCount; i++) {
-    const cardLocator = cardLocators.locator(`nth=${i}`);
+    const cardLocator = cardLocators.locator(`nth=${i} >> a`);
+    // カードのテキストを取得する
     const cardText = await cardLocator.textContent();
+    // クリックイベントで、ページ遷移させる
+    await cardLocator.click();
+    // 会社名を取得する
+    const companyLocator = page.locator('.card-title.company');
+
+    const companyText = await companyLocator.textContent();
     // 下記の書き方で、配列にプッシュ出来る。(が後にCSVに保存したいため、オブジェクト形式でプッシュする事とする。
     // fetchedCards.push(cardText);[
     // 以下のように、キーを設定して値を保存すれば配列にオブジェクトを保存する形となる
     fetchedCards.push({
-      name: cardText // 配列の中身 https://gyazo.com/887e18861f1d56edec44b88ba3ca0ed2/raw
+      // 最初の段階の配列の中身 https://gyazo.com/887e18861f1d56edec44b88ba3ca0ed2/raw
+      company: companyText,
+      name: cardText
     });
+
+    // 複数のページに跨って情報を取得する場合、戻るボタンをクリックして前のページに戻る必要がある
+    // 例: 1つ目のカードをクリックして、1つ目のカード詳細に遷移し、情報を取得した後、1ページ目に戻って、2つ目のカードをクリックして、2つ目のカード詳細に遷移する。これを要素数分繰り返す
+    const backLocator = page.locator('text=戻る');
+    await backLocator.click();
   }
 
   await browser.close();
--- node/node-js-code-mafia/course/04_automation/010_playwright/080_practice_error_handling/start/Example.mjs
+++ node/node-js-code-mafia/course/04_automation/010_playwright/080_practice_error_handling/start/Example.mjs
@@ -1,17 +1,66 @@
-import { chromium } from "@playwright/test";
+import { chromium } from '@playwright/test';
+import * as fs from 'fs';
+import { Parser } from 'json2csv';
 
 /**
  * 練習問題
  * 3ページ目の役職が係長の人物名と会社名をすべてtest-data.csvに出力しなさい。
  * ※会社名が取れない場合にも処理が止まらないように例外処理を追加してください。
- * 
+ *
  * "company","name"
  * "山本金属株式会社","28 伊藤 友美"
  */
 (async () => {
-  const browser = await chromium.launch();
+  const browser = await chromium.launch({ headless: false, slowMo: 500 });
   const page = await browser.newPage();
-  await page.goto(process.env.TARGET_URL);
+  // process.envの使い方と「.env」ファイルを後で
+  // await page.goto(process.env.TARGET_URL);
+  await page.goto('http://localhost:3000');
+  const paginationLocator = page.locator('button.page-number').nth(2);
+  console.log(paginationLocator);
+  await paginationLocator.click();
+
+  const cardLocators = page.locator('.cards.list-group-item');
+  const cardCount = await cardLocators.count();
+
+  const fetchedCards = [];
+  for (let i = 0; i < cardCount; i++) {
+    const cardLocator = cardLocators.locator(`nth=${i} >> a`);
+    await cardLocator.click();
+
+    const divisionLocator = page.locator('.division');
+    const divisionText = await divisionLocator.textContent();
+    const nameLocator = page.locator('.name');
+    const nameText = await nameLocator.textContent();
+    let companyText = '';
+
+    try {
+      const companyLocator = page.locator('.company');
+      companyText = await companyLocator.textContent();
+    } catch (error) {
+      console.error('会社名の取得に失敗しました。');
+    }
+
+    console.log(
+      `company: ${companyText}, division: ${divisionText}, name: ${nameText}`
+    );
+
+    // 係長の時だけ配列に追加する
+    // このやり方だと、係長以外の時もコードが全て処理されるから、不要な処理が走ってしまう。
+    if (divisionText.includes('係長')) {
+      fetchedCards.push({
+        company: companyText,
+        name: nameText
+      });
+    }
+    const backLocator = page.locator('text=戻る');
+    await backLocator.click();
+  }
 
   await browser.close();
+  const parser = new Parser();
+  const csv = parser.parse(fetchedCards);
+  console.log(csv);
+
+  fs.writeFileSync('./practice080-data.csv', csv);
 })();
