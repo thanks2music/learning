aa4c80d1eb589ebf47f5d0266f8613f41dd8ea6c
Yoshiyuki Ito <thanks2music@gmail.com>
2025-03-17 10:24:11 +0900

    Node.jsで立ち上げたサーバー内で、リクエストパスに応じて処理を変える (89)

--- node/node-js-code-mafia/course/06_backend/020_path_control/start/app.mjs
+++ node/node-js-code-mafia/course/06_backend/020_path_control/start/app.mjs
@@ -1,8 +1,18 @@
-import * as http from "http";
+import * as http from 'http';
 
-const server = http.createServer(function(req, res) {
-    console.log(req.url);
-    res.end('bye');
+// リクエストパスに応じて処理を変える
+const server = http.createServer(function (req, res) {
+  if (req.url === '/hello') {
+    res.end('Hello');
+    return;
+  }
+
+  if (req.url === '/bye') {
+    res.end('Bye');
+    return;
+  }
+
+  console.log('Request URL is ', req.url);
 });
 
 server.listen(8080);
