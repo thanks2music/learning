c191d85b0e9534c25675d13483491e70a6792b0a
Yoshiyuki Ito <thanks2music@gmail.com>
2025-04-30 11:06:58 +0900

    Bookshelfの詳細取得と、DELETEメソッドを追加したとこまで (126)

--- node/node-js-code-mafia/course/08_bookshelf_backend/start/server/api-routes/books.mjs
+++ node/node-js-code-mafia/course/08_bookshelf_backend/start/server/api-routes/books.mjs
@@ -4,9 +4,26 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   // MongoDBのメソッドが使用出来る。「find()」で全件を取得する
-  const books = await Book.find();
+  const books = await Book.find().sort({ createdAt: -1 });
   res.json(books);
   // res.send('/api/books GET OK');
+});
+router.get('/:id', async (req, res) => {
+  const _id = req.params.id;
+  // Mongooseの場合、ObjectIdを使わずに、直接IDを指定して取得することができる
+  const books = await Book.find({ _id: _id });
+  // 「findById()」を使うと、IDを指定して取得することができる
+  // const books = await Book.findById(_id);
+  res.json(books);
+  // res.send('/api/books GET OK');
+});
+
+// Deleteメソッド
+router.delete('/:id', async (req, res) => {
+  const _id = req.params.id;
+  // Mongooseの場合、ObjectIdを使わずに、直接IDを指定して取得することができる
+  await Book.deleteOne({ _id: _id });
+  res.json({ msg: 'Delete Succeeded' });
 });
 
 export default router;
--- node/node-js-code-mafia/course/08_bookshelf_backend/start/server/models/book.mjs
+++ node/node-js-code-mafia/course/08_bookshelf_backend/start/server/models/book.mjs
@@ -1,32 +1,36 @@
 import { Schema, model } from 'mongoose';
 
-const bookSchema = Schema({
-  title: {
-    type: String,
-    required: true
+const bookSchema = Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    deescription: {
+      type: String,
+      required: true
+    },
+    comment: {
+      type: String,
+      required: true
+    },
+    rating: {
+      type: Number,
+      enum: [1, 2, 3, 4, 5],
+      required: true,
+      // 取得する値を丸める
+      // 下記の記述は、引数が1つと処理が1行なので、()と{}を省略出来る
+      // get: function(v) {
+      //   return Math.round(v);
+      // },
+      // 暗黙のリターンで以下のように書ける。
+      get: v => Math.round(v),
+      set: v => Math.round(v)
+    }
   },
-  deescription: {
-    type: String,
-    required: true
-  },
-  comment: {
-    type: String,
-    required: true
-  },
-  rating: {
-    type: Number,
-    enum: [1, 2, 3, 4, 5],
-    required: true,
-    // 取得する値を丸める
-    // 下記の記述は、引数が1つと処理が1行なので、()と{}を省略出来る
-    // get: function(v) {
-    //   return Math.round(v);
-    // },
-    // 暗黙のリターンで以下のように書ける。
-    get: v => Math.round(v),
-    set: v => Math.round(v)
-  }
-});
+  { timestamps: true } // createdAtとupdatedAtを自動で追加してくれる
+);
 
+// grepで引っかかりやすいように一度変数に格納しておく
 const Book = model('Book', bookSchema);
 export default Book;
