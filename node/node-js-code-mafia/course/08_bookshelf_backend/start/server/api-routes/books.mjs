66e32bbd879ec58236ed2d4abac3e7afcfa43d79
Yoshiyuki Ito <thanks2music@gmail.com>
2025-04-30 11:28:44 +0900

    PATCHメソッドを追加、分割代入とundefinedを考慮して追加まで (126)

--- node/node-js-code-mafia/course/08_bookshelf_backend/start/server/api-routes/books.mjs
+++ node/node-js-code-mafia/course/08_bookshelf_backend/start/server/api-routes/books.mjs
@@ -11,10 +11,10 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   const _id = req.params.id;
   // Mongooseの場合、ObjectIdを使わずに、直接IDを指定して取得することができる
-  const books = await Book.find({ _id: _id });
+  const book = await Book.find({ _id: _id });
   // 「findById()」を使うと、IDを指定して取得することができる
   // const books = await Book.findById(_id);
-  res.json(books);
+  res.json(book);
 });
 
 // Deleteメソッド
@@ -30,10 +30,25 @@ router.post('/', async (req, res) => {
   const book = new Book(req.body);
   const newBook = await book.save();
   res.json(newBook);
-  // const _id = req.params.id;
-  // Mongooseの場合、ObjectIdを使わずに、直接IDを指定して取得することができる
-  // const books = await Book.find({ _id: _id });
-  // res.json(books);
+});
+
+// PATCHメソッド (更新/
+router.patch('/:id', async (req, res) => {
+  const { title, description, comment, rating } = req.body;
+  const _id = req.params.id;
+  const book = await Book.findById(_id);
+  // データが全て揃って届くとは限らないため、歯抜けで情報が飛んできても、
+  // 届いた情報のみを更新出来るように、サーバー側は準備しておく方が良い。
+  // if (title !== undefined) {
+  //   book.title = title;
+  // }
+  // 上記のように条件式の中の処理が、1行だけなら以下のように省略する事が出来る
+  if (title !== undefined) book.title = title;
+  if (description !== undefined) book.description = description;
+  if (comment !== undefined) book.comment = comment;
+  if (rating !== undefined) book.rating = rating;
+  await book.save();
+  res.json(book);
 });
 
 export default router;
